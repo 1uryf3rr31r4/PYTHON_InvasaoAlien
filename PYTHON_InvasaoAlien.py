0a34367375987c3b5f163ba15aa7226a4e5aa1fa
1uryf3rr31r4 <1uryf3rr31r4@gmail.com>
2025-03-03 17:02:12 -0300

    Atualização de Interface

--- PYTHON_InvasaoAlien.py
+++ PYTHON_InvasaoAlien.py
@@ -15,7 +15,7 @@ class Jogo(Widget):
         self.nave = Rectangle(size=(50,30), pos=(Window.width // 2 - 25, 50))
         self.balas = []
         self.inimigos = []
-        self.score = 0
+        self.placar = 0
 
         self.criar_inimigos()
 
@@ -35,7 +35,7 @@ class Jogo(Widget):
                 inimigo = Rectangle(size=(50,30), pos=(pos_x, pos_y))
                 self.inimigos.append(inimigo)
 
-    def mover_nave(self):
+    def mover_nave(self, instance, touch):
         if touch.x > self.nave.pos[0] and touch.x < self.nave.pos[0] + self.nave.size[0]:
             self.nave.pos = (touch.x - self.nave.size[0] / 2, self.nave.pos[1])
 
@@ -53,7 +53,41 @@ class Jogo(Widget):
                 self.balas.remove(bala)
 
         for inimigo in self.inimigos[:]:
+            inimigo.pos = (inimigo.pos[0], inimigo.pos[1] - self.velocidade_inimigos)
 
+            if inimigo.pos[1] < 50:
+                self.inimigos.remove(inimigo)
+
+            for bala in self.balas[:]:
+                if (bala.pos[0] < inimigo.pos[0] + inimigo.size[0] and
+                    bala.pos[0] + bala.size[0] > inimigo.pos[0] and
+                    bala.pos[1] < inimigo.pos[1] + inimigo.size[1] and
+                    bala.pos[1] + bala.size[1] > inimigo.pos[1]):
+
+                    self.balas.remove(bala)
+                    self.inimigos.remove(inimigo)
+                    self.score += 1
+                    break
+
+        self.canvas.clear()
+        with self.canvas:
+            Color(0,1,0)
+            self.nave
+
+            Color(1,0,0)
+            for bala in self.balas:
+                bala
+
+            Color(1,1,0)
+            for inimigo in self.inimigos:
+                inimigo
+
+            self.desenhar_placar()
+
+    def desenhar_placar(self):
+        placar_label = Label(text=f'Placar: {self.placar}', font_size=20, color=(1,1,1,1))
+        placar_label.pos = (10,Window.height - 30)
+        self.add_widget(placar_label)
 
 class InvasorApp(App):
     def build(self):
